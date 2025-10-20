5135215b12d4670f7e12f1479c961564d45d4d0c
sflwa <philip@southfloridawebadvisors.com>
2025-10-20 16:31:59 -0400

    Update mainwp-fluentsupport.js

--- js/mainwp-fluentsupport.js
+++ js/mainwp-fluentsupport.js
@@ -1,9 +1,15 @@
 jQuery(document).ready(function($) {
 
     var $fetchButton = $('#mainwp-fluentsupport-fetch-btn');
+    var $saveButton = $('#mainwp-fluentsupport-save-settings-btn');
     var $ticketDataBody = $('#fluentsupport-ticket-data');
     var $messageDiv = $('#mainwp-fluentsupport-message');
+    var fetchAction = 'mainwp_fluentsupport_fetch_tickets';
+    var saveAction = 'mainwp_fluentsupport_save_settings';
 
+    // -------------------------
+    // 1. FETCH TICKETS LOGIC
+    // -------------------------
     $fetchButton.on('click', function(e) {
         e.preventDefault();
         
@@ -11,18 +17,17 @@ jQuery(document).ready(function($) {
         $messageDiv.hide().removeClass().empty();
         
         // Initial loading state
-        $ticketDataBody.html('<tr class="loading-row"><td colspan="4"><i class="fa fa-spinner fa-pulse"></i> Retrieving data from all child sites, please wait...</td></tr>');
+        $ticketDataBody.html('<tr class="loading-row"><td colspan="5"><i class="fa fa-spinner fa-pulse"></i> Retrieving data from Support Site...</td></tr>');
 
         $.ajax({
             url: mainwpFluentSupport.ajaxurl,
             type: 'POST',
             data: {
-                action: mainwpFluentSupport.action,
+                action: fetchAction,
                 security: mainwpFluentSupport.nonce
             },
             success: function(response) {
                 if (response.success) {
-                    // Update the table with the returned HTML
                     $ticketDataBody.html(response.data.html);
                     
                     $messageDiv
@@ -30,18 +35,17 @@ jQuery(document).ready(function($) {
                         .html('<strong>Success!</strong> Ticket data updated.')
                         .slideDown();
                 } else {
-                    // Handle AJAX failure (e.g., bad nonce, server error before hitting the core logic)
-                    $ticketDataBody.html('<tr><td colspan="4">An error occurred while fetching data. Please check your MainWP sync status.</td></tr>');
+                    // Update table with the error message from the PHP error response
+                    $ticketDataBody.html(response.data.html || '<tr><td colspan="5">An error occurred while communicating with the Support Site.</td></tr>');
                     
                     $messageDiv
                         .addClass('mainwp-notice mainwp-notice-red')
-                        .html('<strong>Error:</strong> ' + (response.data.message || 'Unknown AJAX Error'))
+                        .html('<strong>Error:</strong> ' + (response.data.message || 'Unknown Error'))
                         .slideDown();
                 }
             },
             error: function(jqXHR, textStatus, errorThrown) {
-                // Handle network or fatal PHP errors
-                $ticketDataBody.html('<tr><td colspan="4">Network or server error: ' + textStatus + '</td></tr>');
+                $ticketDataBody.html('<tr><td colspan="5">Network or server error: ' + textStatus + '</td></tr>');
                 
                 $messageDiv
                     .addClass('mainwp-notice mainwp-notice-red')
@@ -49,8 +53,53 @@ jQuery(document).ready(function($) {
                     .slideDown();
             },
             complete: function() {
-                $fetchButton.prop('disabled', false).text('Fetch All Ticket Data');
+                $fetchButton.prop('disabled', false).text('Fetch Latest Tickets');
             }
         });
     });
+    
+    // -------------------------
+    // 2. SAVE SETTINGS LOGIC
+    // -------------------------
+    $saveButton.on('click', function(e) {
+        e.preventDefault();
+
+        $saveButton.prop('disabled', true).text('Saving...');
+        $messageDiv.hide().removeClass().empty();
+
+        var formData = {
+            action: saveAction,
+            security: mainwpFluentSupport.nonce,
+            fluentsupport_site_id: $('#fluentsupport_site_id').val()
+        };
+
+        $.ajax({
+            url: mainwpFluentSupport.ajaxurl,
+            type: 'POST',
+            data: formData,
+            success: function(response) {
+                if (response.success) {
+                    $messageDiv
+                        .addClass('mainwp-notice mainwp-notice-green')
+                        .html('<strong>Success!</strong> ' + response.data.message)
+                        .slideDown();
+                } else {
+                    $messageDiv
+                        .addClass('mainwp-notice mainwp-notice-red')
+                        .html('<strong>Error:</strong> ' + response.data.message)
+                        .slideDown();
+                }
+            },
+            error: function() {
+                $messageDiv
+                    .addClass('mainwp-notice mainwp-notice-red')
+                    .html('<strong>Fatal Error:</strong> Failed to connect to save settings.')
+                    .slideDown();
+            },
+            complete: function() {
+                $saveButton.prop('disabled', false).text('Save Settings');
+            }
+        });
+    });
+
 });
