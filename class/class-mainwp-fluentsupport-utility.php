957b1e51997347d0dbab0e03270011c83d542c98
sflwa <philip@southfloridawebadvisors.com>
2025-10-21 09:17:55 -0400

    Update class-mainwp-fluentsupport-utility.php

--- class/class-mainwp-fluentsupport-utility.php
+++ class/class-mainwp-fluentsupport-utility.php
@@ -25,10 +25,13 @@ class MainWP_FluentSupport_Utility {
     /**
      * Get Websites
      * Gets all child sites through the 'mainwp_getsites' filter.
+     * * 🔑 FIX: Passing an empty string for the childKey to satisfy the filter requirement 
+     * for non-commercial extensions, resolving the "Total Sites Found: 0" issue.
      */
 	public static function get_websites( $site_id = null ) {
 		global $mainWPFluentSupportExtensionActivator;
-		return apply_filters( 'mainwp_getsites', $mainWPFluentSupportExtensionActivator->get_child_file(), $mainWPFluentSupportExtensionActivator->get_child_key(), $site_id, false );
+        // Use empty string '' instead of the default false returned by get_child_key()
+		return apply_filters( 'mainwp_getsites', $mainWPFluentSupportExtensionActivator->get_child_file(), '', $site_id, false );
 	}
     
     // ... (Add other utility methods here if needed)
